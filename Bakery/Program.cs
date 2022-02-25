66d1b066c16aaaf4395e1f70028a0f6479245afd
Jase Seeley <jcseeley31590@yahoo.com>
2022-02-25 13:09:44 -0800

    receive number of loaves from user.

---
+++ Bakery/Program.cs
@@ -0,0 +1,15 @@
+using Bakery.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Bakery
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to Pierre's Bakery!\nBread is $5 per loaf or Buy 2 Get 1 FREE!\nPastries are $2 each or 3 for $5!\nHow many loaves of bread would you like?");
+      int numberOfLoaves = int.Parse(Console.ReadLine());
+    }
+  }
+}
