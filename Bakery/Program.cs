9f6a441cd4745a2be1cd667fb1fc706c844dbde0
Jase Seeley <jcseeley31590@yahoo.com>
2022-02-25 13:12:17 -0800

    receive number of pastries from user.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -10,6 +10,8 @@ namespace Bakery
     {
       Console.WriteLine("Welcome to Pierre's Bakery!\nBread is $5 per loaf or Buy 2 Get 1 FREE!\nPastries are $2 each or 3 for $5!\nHow many loaves of bread would you like?");
       int numberOfLoaves = int.Parse(Console.ReadLine());
+      Console.WriteLine("And how many pastries for you today?");
+      int numberOfPastries = int.Parse(Console.ReadLine());
     }
   }
 }
