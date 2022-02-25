61e9e19beed600487703217eb3fcc8593080b8f8
Jase Seeley <jcseeley31590@yahoo.com>
2022-02-25 12:44:05 -0800

    pass test for PastryPrice() with more than 3 pastries. All tests passing.

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -12,13 +12,21 @@ namespace Bakery.Models
 
     public int PastryPrice()
     {
-      if (NumberOfPastries == 3)
+      if (NumberOfPastries <= 2)
       {
-        return 5;
+        return NumberOfPastries * 2;
+      }
+      else if (NumberOfPastries % 3 == 0)
+      {
+        return NumberOfPastries / 3 * 5; 
+      }
+      else if (NumberOfPastries % 3 == 2)
+      {
+        return (NumberOfPastries - 2) / 3 * 5 + 4;
       }
       else
       {
-        return NumberOfPastries * 2;
+        return (NumberOfPastries - 1) / 3 * 5 + 2;
       }
     }
   }
