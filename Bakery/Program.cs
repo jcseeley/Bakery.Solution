71e8b94c3a76da00f80ec1ed6f2a5eadbb5901e1
Jase Seeley <jcseeley31590@yahoo.com>
2022-02-26 11:08:29 -0800

    change animation and text outputs.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -9,25 +9,26 @@ namespace Bakery
     public static void Main()
     {
       Console.ForegroundColor = ConsoleColor.Blue;
-      Console.WriteLine("Welcome to Pierre's Bakery!\n");
+      Console.WriteLine("\nWELCOME TO PIERRE'S BAKERY!\n---------------------------");
       Console.ForegroundColor = ConsoleColor.Yellow;
-      Console.WriteLine("-Bread is $5 per loaf or Buy 2 Get 1 FREE!\n-Pastries are $2 each or 3 for $5!\n");
+      Console.WriteLine("-Bread is $5 per loaf or Buy 2 Get 1 FREE!\n-Pastries are $2 each or 3 for $5!\n------------------------------------------");
       Console.ForegroundColor = ConsoleColor.DarkMagenta;
       Console.WriteLine("How many loaves of bread would you like?");
       int numberOfLoaves = int.Parse(Console.ReadLine());
       Console.ForegroundColor = ConsoleColor.DarkYellow;
-      Console.WriteLine("And how many pastries for you today?");
+      Console.WriteLine("------------------------------------\nAnd how many pastries for you today?");
       int numberOfPastries = int.Parse(Console.ReadLine());
       Bread userBread = new Bread(numberOfLoaves);
       Pastry userPastry = new Pastry(numberOfPastries);
+      Console.ForegroundColor = ConsoleColor.Red;
       CalculatingOrder();
       int breadTotal = userBread.BreadPrice();
       int pastryTotal = userPastry.PastryPrice();
       int result = breadTotal + pastryTotal;
       Console.ForegroundColor = ConsoleColor.Green;
-      Console.WriteLine("\n" + userBread.NumberOfLoaves + " x Bread: $" + breadTotal + "\n" + userPastry.NumberOfPastries + " x Pastries: $" + pastryTotal + "\n--------------------" + "\nOrder Total: $" + result + "\n");
+      Console.WriteLine("\nYOUR ORDER:\n--------------------\n" + userBread.NumberOfLoaves + " x Bread: $" + breadTotal + "\n" + userPastry.NumberOfPastries + " x Pastries: $" + pastryTotal + "\n--------------------" + "\nTotal: $" + result + "\n");
       Console.ForegroundColor = ConsoleColor.Yellow;
-      Console.WriteLine("Would you like to place another order? Type 'Y' for yes or 'N' for no.");
+      Console.WriteLine("----------------------------------------------------------------------\nWould you like to place another order? Type 'Y' for yes or 'N' for no.");
       string response = Console.ReadLine();
       if (response == "y" || response == "Y")
       {
@@ -36,44 +37,68 @@ namespace Bakery
       else
       {
         Console.ForegroundColor = ConsoleColor.Blue;
-        Console.WriteLine("Thank you for visiting Pierre's!\n");
+        Console.WriteLine("--------------------------------\nTHANK YOU FOR VISITING PIERRE'S!\n--------------------------------");
       }
 
       static void CalculatingOrder()
       {
         int counter = 0;
-        for (int i = 0; i <= 25; i++)
+        for (int i = 0; i <= 47; i++)
         {
-          switch (counter % 23)
+          switch (counter % 47)
           {
-            case 0: Console.Write($"\r"); break;
-            case 1: Console.Write($"\rC"); break;
-            case 2: Console.Write($"\rCA"); break;
-            case 3: Console.Write($"\rCAL"); break;
-            case 4: Console.Write($"\rCALC"); break;
-            case 5: Console.Write($"\rCALCU"); break;
-            case 6: Console.Write($"\rCALCUL"); break;
-            case 7: Console.Write($"\rCALCULA"); break;
-            case 8: Console.Write($"\rCALCULAT"); break;
-            case 9: Console.Write($"\rCALCULATI"); break;
-            case 10: Console.Write($"\rCALCULATIN"); break;
-            case 11: Console.Write($"\rCALCULATING"); break;
-            case 12: Console.Write($"\rCALCULATIN"); break;
-            case 13: Console.Write($"\rCALCULATI"); break;
-            case 14: Console.Write($"\rCALCULAT"); break;
-            case 15: Console.Write($"\rCALCULA"); break;
-            case 16: Console.Write($"\rCALCUL"); break;
-            case 17: Console.Write($"\rCALCU"); break;
-            case 18: Console.Write($"\rCALC"); break;
-            case 19: Console.Write($"\rCAL"); break;
-            case 20: Console.Write($"\rCA"); break;
-            case 21: Console.Write($"\rC"); break;
-            case 22: Console.Write($"\r"); break;
+            case 0: Console.Write($"\rD"); break;
+            case 1: Console.Write($"\rDE"); break;
+            case 2: Console.Write($"\rDET"); break;
+            case 3: Console.Write($"\rDETE"); break;
+            case 4: Console.Write($"\rDETER"); break;
+            case 5: Console.Write($"\rDETERM"); break;
+            case 6: Console.Write($"\rDETERMI"); break;
+            case 7: Console.Write($"\rDETERMIN"); break;
+            case 8: Console.Write($"\rDETERMINI"); break;
+            case 9: Console.Write($"\rDETERMININ"); break;
+            case 10: Console.Write($"\rDETERMINING"); break;
+            case 11: Console.Write($"\rDETERMINING "); break;
+            case 12: Console.Write($"\rDETERMINING H"); break;
+            case 13: Console.Write($"\rDETERMINING HO"); break;
+            case 14: Console.Write($"\rDETERMINING HOW"); break;
+            case 15: Console.Write($"\rDETERMINING HOW "); break;
+            case 16: Console.Write($"\rDETERMINING HOW M"); break;
+            case 17: Console.Write($"\rDETERMINING HOW MU"); break;
+            case 18: Console.Write($"\rDETERMINING HOW MUC"); break;
+            case 19: Console.Write($"\rDETERMINING HOW MUCH"); break;
+            case 20: Console.Write($"\rDETERMINING HOW MUCH "); break;
+            case 21: Console.Write($"\rDETERMINING HOW MUCH '"); break;
+            case 22: Console.Write($"\rDETERMINING HOW MUCH 'B"); break;
+            case 23: Console.Write($"\rDETERMINING HOW MUCH 'BR"); break;
+            case 24: Console.Write($"\rDETERMINING HOW MUCH 'BRE"); break;
+            case 25: Console.Write($"\rDETERMINING HOW MUCH 'BREA"); break;
+            case 26: Console.Write($"\rDETERMINING HOW MUCH 'BREAD"); break;
+            case 27: Console.Write($"\rDETERMINING HOW MUCH 'BREAD'"); break;
+            case 28: Console.Write($"\rDETERMINING HOW MUCH 'BREAD' "); break;
+            case 29: Console.Write($"\rDETERMINING HOW MUCH 'BREAD' Y"); break;
+            case 30: Console.Write($"\rDETERMINING HOW MUCH 'BREAD' YO"); break;
+            case 31: Console.Write($"\rDETERMINING HOW MUCH 'BREAD' YOU"); break;
+            case 32: Console.Write($"\rDETERMINING HOW MUCH 'BREAD' YOU "); break;
+            case 33: Console.Write($"\rDETERMINING HOW MUCH 'BREAD' YOU O"); break;
+            case 34: Console.Write($"\rDETERMINING HOW MUCH 'BREAD' YOU OW"); break;
+            case 35: Console.Write($"\rDETERMINING HOW MUCH 'BREAD' YOU OWE"); break;
+            case 36: Console.Write($"\rDETERMINING HOW MUCH 'BREAD' YOU OWE "); break;
+            case 37: Console.Write($"\rDETERMINING HOW MUCH 'BREAD' YOU OWE U"); break;
+            case 38: Console.Write($"\rDETERMINING HOW MUCH 'BREAD' YOU OWE US"); break;
+            case 39: Console.Write($"\rDETERMINING HOW MUCH 'BREAD' YOU OWE US"); break;
+            case 40: Console.Write($"\rDETERMINING HOW MUCH 'BREAD' YOU OWE US H"); break;
+            case 41: Console.Write($"\rDETERMINING HOW MUCH 'BREAD' YOU OWE US HA"); break;
+            case 42: Console.Write($"\rDETERMINING HOW MUCH 'BREAD' YOU OWE US HA "); break;
+            case 43: Console.Write($"\rDETERMINING HOW MUCH 'BREAD' YOU OWE US HA H"); break;
+            case 44: Console.Write($"\rDETERMINING HOW MUCH 'BREAD' YOU OWE US HA HA"); break;
+            case 45: Console.Write($"\rDETERMINING HOW MUCH 'BREAD' YOU OWE US HA HA H"); break;
+            case 46: Console.Write($"\rDETERMINING HOW MUCH 'BREAD' YOU OWE US HA HA HA"); break;
           }
           counter++;
-          Thread.Sleep(75);
+          Thread.Sleep(125);
         }
-        Console.Write($"\r               ");
+        Console.Write($"\r                                                ");
       }
     }
   }
